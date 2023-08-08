449d66358593c5f9f6c440f4b09c8704b62cdd3b
CrimsonCloak <alexander.veldeman.999@gmail.com>
2023-08-08 11:52:59 +0200

    Added full name concat for Name in table

--- provisioning/files/nameslist.php
+++ provisioning/files/nameslist.php
@@ -10,7 +10,7 @@
 <style><?php include 'styles.css'; ?></style>
 <table>
   <tr>
-    <th>First name</th>
+    <th>Name</th>
   </tr>
 <?php
 echo "<h2>Employees</h2>";
@@ -22,10 +22,11 @@ $table = "people";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
 
-  $data = $db->query("SELECT FirstName FROM $table")->fetchAll();
+  $data = $db->query("SELECT FirstName, LastName FROM $table")->fetchAll();
   foreach ($data as $row) {
     echo "<tr> <td>";
     echo $row['FirstName'] . "<br>";
+    echo $row['LastName']. "<br>";
     echo "</td> </tr>";
 } 
 } catch (PDOException $e) {
@@ -37,14 +38,32 @@ try {
 
 $db = null;
 ?>
-  
-
-
-    <!-- <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td> -->
   </tr>
 </table>
+
+
+<!-- 
+<form action="#" method="post">
+
+<?php
+ $FirstName = $LastName = $message = "";
+?>
+<label for="Name">Name:</label> <br>
+<input type="text" id="Name" name="Name" required>
+<br>
+
+<label for="Email">Email address:</label> <br>
+<input type="text" name="Email" id="Email" required> <br>
+
+<label for="Updates">I would like to receive updates and news related to my PHP skills.</label> <br>
+<input type="checkbox" name="Updates" id="Updates"><br>
+
+<input type="submit" value="Send">
+</form>
+
+<?php
+require 'contact_form.php';
+?> -->
 
 
 
